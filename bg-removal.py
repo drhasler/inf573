6b9e7f2f2bd2e1190b1304a44e26312ec9185bee
Matthias Hasler <matthias@hasler.fr>
2020-11-19 13:48:15 +0100

    bg removed

--- bg-removal.py
+++ bg-removal.py
@@ -2,22 +2,56 @@ import numpy as np
 import cv2
 
 from skimage.measure import label
+from skimage.morphology import skeletonize
 
 cap = cv2.VideoCapture(0) # cam feed
-backSub = cv2.createBackgroundSubtractorMOG2()
+# backSub = cv2.createBackgroundSubtractorMOG2()
+ker = np.ones((25,25),np.uint8)
 
-ker = np.ones((5,5),np.uint8)
+def mask_th(img, th=0):
+    return 255 * np.uint8(img > th)
 
-while True:
-    ret, frame = cap.read()
+def mog(frame):
     fgm = backSub.apply(frame)
-    fgm = 255 * np.uint8(fgm > 0)
+    fgm = mask_th(fgm)
     fgm = cv2.dilate(fgm, ker)
     comp = label(fgm)
     comp = np.uint8(comp)
     cmap = cv2.applyColorMap(comp, 10)
+    return cmap
+
+bgimg = None
+def diff(frame):
+    if bgimg is None: return frame
+    d = cv2.absdiff(frame, bgimg)
+    m = mask_th(d, 10)
+    m = cv2.erode(cv2.dilate(m, ker), ker) # closing
+    m = connected_comp(m)
+    return m
+    # skeleton = skeletonize(c)
+    # return skeleton
+    return frame * d
+
+def connected_comp(mask):
+    labels = label(mask)
+    labels = np.uint8(labels)
+    cmap = cv2.applyColorMap(labels, 2)
+    x = np.argsort(np.bincount(labels.flat))
+    return labels == x[-2]
+    return cmap
+
+while True:
+    ret, frame = cap.read()
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    out = diff(frame)
+
+    out = 255*np.uint8(out)
 
     cv2.imshow('cam1', frame)
-    cv2.imshow('mask', cmap)
-    cv2.waitKey(1)
+    cv2.imshow('out', out)
+    k = cv2.waitKey(1)
+    if k == ord('b'):
+        bgimg = frame
+    if k == ord('q'):
+        break
 
