69befa4bf1a6831ecedf6f52245518a32bc36cc2
Guillaume.Loranchet <guillaume.loranchet@polytechnique.edu>
2020-12-15 17:51:38 +0100

    try client

--- src/client.py
+++ src/client.py
@@ -18,7 +18,7 @@ sender = imagezmq.ImageSender(connect_to="tcp://{}:5555".format(
 # camera sensor to warmup
 rpiName = socket.gethostname()
 # vs = VideoStream(usePiCamera=True).start()
-vs = VideoStream(src=0).start()
+vs = VideoStream(src=0,resolution=(320, 240)).start()
 time.sleep(2.0)
  
 while True:
